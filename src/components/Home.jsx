20ed7db077bda8ab411e7f952a5375eb6b5518cc
Carol <cgsze2@gmail.com>
2025-11-01 21:08:23 -0500

    added wise logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
-export default function AboutUs (props) {
+import wise_logo from '../assets/wise-logo-color.png';
+export default function Home (props) {
     return <div>
         <h1>Home!</h1>
+        <img src={wise_logo} alt="wise logo"/>
     </div>
 
 }
