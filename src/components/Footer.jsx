56b51337a6d4fcb8e3659ed7f1c64dca7dd3240f
Anna Wang <awang282@wisc.edu>
2025-11-13 22:08:52 -0600

    Added Footer and styling to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import {Container, Row} from "react-bootstrap";
 export default function Footer (props) {
-    return <div>
-    </div>
+    return <footer>
+        <div class ="footerContent">
+            <a href="https://www.housing.wisc.edu/" target="_blank">University Housing</a>
+            <a href = "https://www.instagram.com/wise_uw/" target="_blank">WISE Instagram</a>
+            <a href = "https://www.facebook.com/uwmadison.wise/" target="_blank">WISE Facebook</a>
+            
+        {/* links to uw housing, wise instagram, stay connected page */}
+        </div>
+    </footer>
 
 }
