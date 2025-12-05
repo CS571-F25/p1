bfe699e8d12c5c621692320166345adc1f6c4b38
Anna Wang <awang282@wisc.edu>
2025-12-05 09:42:15 -0600

    Edited modal text and date of one of events

--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -7,9 +7,8 @@ export default function EventModal(props) {
   // console.log(props)
   const { selectedEvent } = useContext(EventDataContext);
   if (!selectedEvent) return null;
-  // cons
   // console.log(selectedEvent.event_name)
-  const description = `WISE Poster for ${selectedEvent.event_name}`
+  const description = `WISE Poster for ${selectedEvent.event_name}`;
   return (
     
     <Modal
@@ -25,8 +24,9 @@ export default function EventModal(props) {
     </Modal.Header>
     <Modal.Body >
       <img src={selectedEvent.image} className="img-fluid d-block mx-auto" alt={description} height={275} width={275} /> 
-      <p>Join WISE on {selectedEvent.date} at {selectedEvent.start_time} for {selectedEvent.event_name}!</p>
+      <h3>Join WISE on {selectedEvent.date} at {selectedEvent.start_time} for {selectedEvent.event_name}!</h3>
       <p>{selectedEvent.event_desc}</p>
+      
     </Modal.Body>
     <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
